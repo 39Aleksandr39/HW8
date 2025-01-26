1648a39a73dd6e69c62d4532bbc45b8d2e632bc3
Alex <deaddemon39@list.ru>
2025-01-26 16:29:32 +0000

    HW8

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,33 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+
+        int array[] = {11, 12, 22, 32, 5, 8, 21, 13, 1414, 55};
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            System.out.println(array[i]);
+//        }
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i]; // TEMP временная переменная для i
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = temp;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+
+//        double[] peopleWeights = new double[10];
+//        for (int i = 0; i < peopleWeights.length; i++) {
+//            peopleWeights[i] = (new Random().nextDouble() * 60) + 40;
+//        }
+//        double middleWeight =0;
+//        for (int i = 0; i < peopleWeights.length; i++) {
+//            middleWeight += peopleWeights[i];
+//        }
+//
+//        for (int i = 0; i < peopleWeights.length; i++) {
+//            System.out.println(peopleWeights[i]);
+//        }
+//
+//        int peopleCount = 0;
+//        for (int i = 0; i < peopleWeights.length; i++) {
+//            if (peopleWeights[i] >= 60 && peopleWeights[i] <= 80) {
+//                peopleCount++;
+//            }
+//        }
+
+//        System.out.println("Средний вес = " + middleWeight / peopleWeights.length);
+//        System.out.println("Кол-во людей: " + peopleCount);
+
+        System.out.println("УПРОЩЕННЫЙ ВАРИАНТ");
+
+        double[] peopleWeights = new double[10];
+        double middleWeight = 0;
+        int peopleCount = 0;
+
+        for (int i = 0; i < peopleWeights.length; i++) {
+            peopleWeights[i] = (new Random().nextDouble() * 60) + 40;
+            middleWeight += peopleWeights[i];
+            if (peopleWeights[i] >= 60 && peopleWeights[i] <= 80) {
+                peopleCount++;
+            }
+        }
+
+        System.out.println("Средний вес = " + (middleWeight / peopleWeights.length));
+        System.out.println("Кол-во людей: " + peopleCount);
     }
 }
